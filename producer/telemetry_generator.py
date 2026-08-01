9f6d79fda75ce21b080f7ea4922f16cf9218f1a3
Raiba Kate <raibakate36@gmail.com>
2026-08-01 12:53:32 +0530

    Refactor producer with Truck class and fleet simulation

--- producer/telemetry_generator.py
+++ producer/telemetry_generator.py
@@ -1,109 +1,25 @@
-import random
 import json 
 import time 
-from datetime import datetime
+from truck import Truck
 
-truck_state = {
-    "truck_id": "TRUCK-01",
-    "latitude": 18.520430,
-    "longitude": 73.856743,
-    "fuel":100.0,
-    "speed":40,
-    "temperature":30
-}
+fleet = []
 
-def generate_truck_id():
-    return "TRUCK-" + str(random.randint(1, 10)).zfill(2)
-
-def generate_telemetry():
-    truck={
-        "truck_id":truck_state["truck_id"],
-        "speed":truck_state["speed"],
-        "fuel":truck_state["fuel"],
-        "temperature":truck_state["temperature"],
-        "latitude":truck_state["latitude"],
-        "longitude":truck_state["longitude"],
-        "timestamp":datetime.now().isoformat()
-
-    }
-    if truck_state["speed"] == 0:
-        truck["status"] = "STOPPED"
-    else:
-        truck["status"] = "MOVING"
-    
-
-    if truck_state["fuel"] < 15:
-        truck["alert"] = "LOW_FUEL"
-    else:
-        truck["alert"] = "NORMAL"
-
-
-    speed_change = random.randint(-5, 5)
-    truck_state["speed"] += speed_change
-
-    if truck_state["speed"] < 0:
-        truck_state["speed"] = 0
-
-    if truck_state["speed"] > 120:
-        truck_state["speed"] = 120
-
-    
-
-
-
-    speed = truck["speed"]
-
-    if speed == 0:
-        consumption = 0
-    elif speed < 20:
-        consumption = 0.005
-    elif speed < 60:
-        consumption = 0.02
-    else:
-        consumption = 0.04
-
-    truck_state["fuel"] = round(
-    max(0, truck_state["fuel"] - consumption),
-    2
-    )
-    
-    
-
-    if truck_state["speed"] == 0:
-        truck_state["temperature"] -= 0.1
-    elif truck_state["speed"] < 40:
-        truck_state["temperature"] += 0.05
-    elif truck_state["speed"] < 80:
-        truck_state["temperature"] += 0.1
-    else:
-        truck_state["temperature"] += 0.2
-
-    truck_state["temperature"] = round(
-    max(20, min(45, truck_state["temperature"])),
-    1
-    )
-    if truck_state["speed"] == 0:
-        movement = 0
-    elif truck_state["speed"] < 40:
-        movement = 0.0001
-    elif truck_state["speed"] < 80:
-        movement = 0.0003
-    else:
-        movement = 0.0005
-
-    truck_state["latitude"] += random.uniform(-movement, movement)
-    truck_state["longitude"] += random.uniform(-movement, movement)
-
-
-
-    return truck
+for i in range(5):
+    fleet.append(Truck(f"TRUCK-{i+1:02d}"))
 
 
 
 while True:
-    telemetry = generate_telemetry()
-    print(json.dumps(telemetry, indent=4))
-    with open("telemetry.json", "a") as file:
-        json.dump(telemetry, file)
-        file.write("\n")
+    for truck in fleet:
+
+        truck.update()
+
+        telemetry = truck.generate_telemetry()
+
+        print(json.dumps(telemetry, indent=4))
+
+        with open("telemetry.json", "a") as file:
+            json.dump(telemetry, file)
+            file.write("\n")
+
     time.sleep(1)
---
+++ producer/truck.py
@@ -0,0 +1,106 @@
+import random
+from datetime import datetime
+
+
+class Truck:
+
+    def __init__(self,truck_id):
+        self.truck_id=truck_id
+        self.latitude=round(random.uniform(18.45, 18.75), 6)
+        self.longitude= round(random.uniform(73.75, 74.05), 6)
+        self.speed=random.randint(35,45)
+        self.fuel=100
+        self.temperature=round(random.uniform(25,35),1)
+
+
+
+    def generate_telemetry(self):
+        truck={
+            "truck_id": self.truck_id,
+        "speed": self.speed,
+        "fuel": self.fuel,
+        "temperature": self.temperature,
+        "latitude": self.latitude,
+        "longitude": self.longitude,
+        "status":self.get_status(),
+        "alert":self.get_alert(),
+        "timestamp": datetime.now().isoformat()
+        }  
+
+        return truck
+
+    def update(self):
+        #Changing Speed
+        speed_change = random.randint(-5, 5)
+        self.speed += speed_change
+
+        if self.speed < 0:
+            self.speed = 0
+
+        if self.speed > 120:
+            self.speed = 120
+        #Changing Fuel
+        if self.speed == 0:
+            consumption = 0
+        elif self.speed < 20:
+            consumption = 0.005
+        elif self.speed < 60:
+            consumption = 0.02
+        else:
+            consumption = 0.04
+
+        self.fuel = round(
+            max(0, self.fuel - consumption),
+            2
+        )
+        #Changing Location(Latitude,Longitude)
+        if self.speed==0:
+            movement=0
+        elif self.speed<40:
+            movement=0.0001
+        elif self.speed<80:
+            movement=0.0003
+        else:
+            movement=0.0005
+
+        self.latitude+=random.uniform(-movement,movement)
+        self.longitude+=random.uniform(-movement,movement)
+
+        #Changing Temperature
+        if self.speed==0:
+            self.temperature -= 0.1
+        elif self.speed<40:
+            self.temperature += 0.05
+        elif self.speed<80:
+            self.temperature += 0.1
+        else:
+            self.temperature += 0.2
+
+        self.temperature=round(max(20,min(45,self.temperature)),1)
+
+
+
+    def get_status(self):
+        if self.speed==0:
+            return "STOPPED"
+        else:
+            return "MOVING"
+
+
+    def get_alert(self):
+
+        alerts=[]
+
+        if self.fuel<15:
+            alerts.append("LOW_FUEL")
+
+        if self.temperature>42:
+            alerts.append("HIGH_ENGINE_TEMPERTURE")
+
+        if self.speed >100:
+            alerts.append("OVERSPEED")
+
+        if not alerts:
+            alerts.append("NORMAL")
+
+        return alerts
